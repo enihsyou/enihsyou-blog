2d7982ca40011e212d192643ed79d95ad472485d
九条涼果 <enihsyou@gmail.com>
2025-10-16 15:45:46 +0800

    从源代码中移除 base64 字符车

--- scripts/hexo-injects.js
+++ scripts/hexo-injects.js
@@ -1,4 +1,5 @@
 'use strict';
+const fs = require('fs');
 
 // Host the umami script locally to avoid being blocked by ad blockers
 // https://umami.is/docs/bypass-ad-blockers#hosting-the-tracker-script
@@ -12,4 +13,30 @@ hexo.extend.generator.register("umami-script", async function (locals) {
     path: "umami.js",
     data: scriptText,
   };
+});
+
+// 避免在源代码中表达 base64 文本，改为生成文件
+hexo.extend.generator.register("travel-moe-svg", async function (locals) {
+  function loadSvg(url) {
+    return fetch(url).then(resp => resp.text());
+  }
+  const lightOption = {
+    "labelColor": "282726",
+  }
+  const darkOption = {
+    "labelColor": "FFFFFF",
+  }
+  const logoBase64 = fs.readFileSync("source/images/travel-moe-logo.png");
+  const logoDataUri = `data:image/png;base64,${logoBase64.toString('base64')}`;
+  function makeUrl(option) {
+    const url = new URL("https://img.shields.io/badge/异次元之旅");
+    url.pathname += "-" + option.labelColor;
+    url.searchParams.set("logo", logoDataUri);
+    return url.toString();
+  }
+
+  return [
+    { path: "images/travel-moe-light.svg", data: await loadSvg(makeUrl(lightOption)) },
+    { path: "images/travel-moe-dark.svg", data: await loadSvg(makeUrl(darkOption)) }
+  ]
 });
